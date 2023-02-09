84d0646d1126defe9941599962dfcf7885f9b010
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-02-09 10:12:49 +0500

    Revision of the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -43,49 +43,66 @@ class HomeScreen extends StatelessWidget {
                   "Choose Your Drink",
                   style: TextStyle(
                       fontSize: 40,
-                      color: Theme
-                          .of(context)
-                          .primaryColor,
+                      color: Theme.of(context).primaryColor,
                       fontWeight: FontWeight.bold),
                 ),
               ),
               Row(
                 children: [
                   Expanded(
-                    child: ReusaContainer("images/coffee1.jpeg", "Americano", "Steam hot foam", "2.5"),
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => OrderScreen(
+                                      "images/coffee1.jpeg",
+                                      "Americano",
+                                      "Steam hot foam",
+                                      2.5)));
+                        },
+                        child: ReusaContainer("images/coffee1.jpeg",
+                            "Americano", "Steam hot foam", "1")),
                   ),
                   Expanded(
-                    child: ReusaContainer("images/coffee2.jpg", "Cappuccino", "Double Hot milk", "3.5"),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ReusaContainer("images/coffee3.jpg", "Flat White", "Steam hot foam", "2.5"),
-                  ),
-                  Expanded(
-                    child: ReusaContainer("images/coffee4.jpg", "Latte", "Double Hot milk", "3.5"),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ReusaContainer("images/coffee5.jpg", "Afforgo", "Steam hot foam", "2.5"),
-                  ),
-                  Expanded(
-                    child: ReusaContainer("images/coffee6.jpg", "Hot Chocolate ", "Double Hot milk", "3.5"),
+                    child: ReusaContainer("images/coffee2.jpg", "Cappuccino",
+                        "Double Hot milk", "4"),
                   ),
                 ],
               ),
               Row(
                 children: [
                   Expanded(
-                    child: ReusaContainer("images/coffee7.jpg", "Flat White", "Steam hot foam", "2.5"),
+                    child: ReusaContainer("images/coffee3.jpg", "Flat White",
+                        "Steam hot foam", "2.5"),
                   ),
                   Expanded(
-                    child: ReusaContainer("images/coffee8.jpg", "Latte", "Double Hot milk", "3.5"),
+                    child: ReusaContainer(
+                        "images/coffee4.jpg", "Latte", "Double Hot milk", "3"),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ReusaContainer(
+                        "images/coffee5.jpg", "Afforgo", "Steam hot foam", "2"),
+                  ),
+                  Expanded(
+                    child: ReusaContainer("images/coffee6.jpg",
+                        "Hot Chocolate ", "Double Hot milk", "3.5"),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ReusaContainer("images/coffee7.jpg", "Espresso",
+                        "Steam hot foam", "1.5"),
+                  ),
+                  Expanded(
+                    child: ReusaContainer("images/coffee8.jpg", "Iced Mocha",
+                        "Double Hot milk", "3"),
                   ),
                 ],
               ),
@@ -113,8 +130,7 @@ class ReusaContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8)),
+          color: Colors.white, borderRadius: BorderRadius.circular(8)),
       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 12),
       child: Column(
         children: [
@@ -125,8 +141,7 @@ class ReusaContainer extends StatelessWidget {
           ),
           Text(
             title,
-            style: TextStyle(
-                fontSize: 28, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           ),
           Text(
             desc,
@@ -134,10 +149,136 @@ class ReusaContainer extends StatelessWidget {
           ),
           Text(
             "\$${price}",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold),
-          )
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class OrderScreen extends StatefulWidget {
+  // const OrderScreen({Key? key}) : super(key: key);
+
+  final image;
+  final title;
+  final descr;
+  final price;
+
+  OrderScreen(this.image, this.title, this.descr, this.price);
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  int amount = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                height: 60,
+                width: 60,
+                margin: EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 2.0),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("images/profile.jpg")),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 21),
+              child: Text(
+                "How many Do you need?",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Image(
+                        height: 140,
+                        fit: BoxFit.fitHeight,
+                        image: AssetImage(widget.image)),
+                    SizedBox(height: 20),
+                    Text(
+                      widget.title,
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      widget.descr,
+                      style: TextStyle(fontSize: 22, color: Colors.white),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "\$${widget.price}",
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      amount--;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Color(0xff8f6251),
+                        borderRadius: BorderRadius.circular(28)),
+                    child: Icon(Icons.remove, color: Colors.white, size: 50),
+                  ),
+                ),
+                Text(amount.toString(), style: TextStyle(fontSize: 60, color: Colors.white),),
+                InkWell(
+                  onTap: (){
+                    setState(() {
+                      amount++;
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        color: Color(0xff8f6251),
+                        borderRadius: BorderRadius.circular(28)),
+                    child: Icon(Icons.add, color: Colors.white, size: 50),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
